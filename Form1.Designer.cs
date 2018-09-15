ffbde73bf8abcbb21c7e6e928f92ecfa93a57906
adriel2018 <AdrielF31@gmail.com>
2018-09-15 15:48:51 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(585, 215);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Unhash";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(46, 213);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Import Hashes";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(122, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(542, 136);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(220, 267);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(349, 96);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged_2);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(782, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "UnHasher 0.3A by | Unkown and Someone";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Net;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1() //im here
+        {
+            InitializeComponent(); //start it and put the hashes in the console ok     opening discord rn
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        
+            
+                //string serverlink;
+                //serverlink = Console.ReadLine();
+                //WebRequest request = WebRequest.Create("http://www.nitrxgen.net/md5db/" + serverlink);
+                //WebResponse response = request.GetResponse();
+                //System.IO.StreamReader reader = new System.IO.StreamReader(response.GetResponseStream());
+                //richTextBox1.Text = serverlink + " = " + reader.ReadToEnd(); //how about now ? none
+                ////Console.WriteLine(serverlink + " = " + reader.ReadToEnd());
+                
+                timer1.Start();
+                MessageBox.Show("Hashes have been Unhashed!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hashes have been Imported!");
+        }
+        
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            
+        } 
+
+        private void richTextBox1_TextChanged_2(object sender, EventArgs e)
+        {
+            //this is it  //what ios the name //you got any errors ??? one where cuz i can see them idk why
+            //Line 31
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            /*
+            You're so good lol
+             btw you got to add that hash at the end of your list c8034a9882c2d012bf887d12f57219b5 for now cuz 
+             im finding a method, I see. So it wil read the list I provide it and always look for "c8034a9882c2d012bf887d12f57219b5" correct?
+             just put the hash at the end of the list
+             then copy and past in console ..
+             Okay, I gotta practice C# more like wayyy more
+             anyway im gonna put the console in c++ it will be easier 
+             Okay, I gotta learn C# fully before moving into C++ lol,
+             c++ its easy to
+             ok, I am happy I kind of helped you with the ui             lol
+             Lemme test it out
+            */
+            if (richTextBox1.Text.Contains("<END>"))
+            {
+                timer1.Stop();
+            }
+            else
+            {
+                string serverlink;
+                serverlink = Console.ReadLine();
+                WebRequest request = WebRequest.Create("http://www.nitrxgen.net/md5db/" + serverlink);
+                WebResponse response = request.GetResponse();
+                System.IO.StreamReader reader = new System.IO.StreamReader(response.GetResponseStream());
+                richTextBox1.AppendText(reader.ReadToEnd() + "\n");
+            }
+        }
+    }
+}
+/*
+Discord crashed gimme 1s
+
+
+*/
